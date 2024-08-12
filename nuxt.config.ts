278b5aba6cf65fbac62105e7d85543558dd5be56
Tolobinha <xtrubulho@protonmail.com>
2024-08-12 15:10:50 -0300

    "chore: nuxt config"

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt', '@nuxt/test-utils/module',],
+  build: {
+    hotMiddleware: {
+      client: {
+        overlay: false
+      }
+    }
+  },
+    compatibilityDate: "2024-08-08",
+
 })
